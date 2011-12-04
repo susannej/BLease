38badb9ba5c22ac28feb57489a4151dc3fa8819d
joe <joe@LW03199.lan>
2011-12-05 00:00:27 +0100

    initial commit

---
+++ BLease-Server/src/blease/Ping.groovy
@@ -0,0 +1,54 @@
+package blease
+
+import javax.xml.stream.XMLStreamException;
+import javax.xml.namespace.QName;
+
+import org.apache.axiom.om.OMAbstractFactory;
+import org.apache.axiom.om.OMElement;
+import org.apache.axiom.om.OMFactory;
+import org.apache.axiom.om.OMNamespace;
+
+import java.util.HashMap;
+
+class Ping {
+
+    private HashMap map = new HashMap();
+
+    private String namespace = "http://quickstart.samples/xsd";
+
+    OMElement getDate(OMElement element) throws XMLStreamException {
+        element.build();
+        element.detach();
+
+        OMElement symbolElement = element.getFirstElement();
+        String symbol = symbolElement.getText();
+
+        String returnText = "42";
+        Double price = (Double) map.get(symbol);
+        if(price != null){
+            returnText  = "" + price.doubleValue();
+        }
+        OMFactory fac = OMAbstractFactory.getOMFactory();
+        OMNamespace omNs =
+            fac.createOMNamespace(namespace, "ns");
+        OMElement method = fac.createOMElement("getPriceResponse", omNs);
+        OMElement value = fac.createOMElement("return", omNs);
+        value.addChild(fac.createOMText(value, returnText));
+        method.addChild(value);
+        return method;
+    }
+
+    void update(OMElement element) throws XMLStreamException {
+        element.build();
+        element.detach();
+
+        OMElement symbolElement = element.getFirstChildWithName(new QName(namespace, "symbol"));
+        String symbol = symbolElement.getText();
+
+        OMElement priceElement = element.getFirstChildWithName(new QName(namespace, "price"));
+        String price = priceElement.getText();
+
+        map.put(symbol, new Double(price));
+    }
+
+}
