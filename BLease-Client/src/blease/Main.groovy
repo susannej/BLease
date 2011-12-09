8be1e5252b5f4534bc2991a9f71951a43b25f914
susannej <susanne_j@gmx.de>
2011-12-09 22:41:59 +0100

    set up a first communication test

--- BLease-Client/src/blease/Main.groovy
+++ BLease-Client/src/blease/Main.groovy
@@ -1,9 +1,19 @@
 package blease
 
+import wslite.soap.*
+
 class Main {
 
-	Main() {
-		
+	static void main(args) {
+		def client = new SOAPClient('http://localhost:8080/axis2/services/BLeaseServer')
+		def response = client.send(SOAPAction:'/GetDate') {
+			body {
+				GetMothersDay('xmlns':'http://www.27seconds.com/Holidays/US/Dates/') {
+					year(2011)
+				}
+			}
+		}
+		println response
 	}
 	
 }
