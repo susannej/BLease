56afcdeff8e95aae8c736f5ca8ed3dd6d8bc0ef1
susannej <susanne_j@gmx.de>
2011-12-12 16:19:20 +0100

    log4j logging added

--- BLease-Server/src/blease/Ping.groovy
+++ BLease-Server/src/blease/Ping.groovy
@@ -1,5 +1,7 @@
 package blease
 
+import groovy.util.logging.Log4j
+
 import org.apache.axiom.om.OMElement
 
 import tools.OMElementBuilder
@@ -7,27 +9,34 @@ import tools.OMElementBuilder
 import static java.text.DateFormat.*
 import static java.util.Locale.*
 
-
+@Log4j
 class Ping {
 
     OMElement getDate(OMElement element) {
 		
-		println element.toString()
+		log.trace "getDate Method"
+		log.info element.toString()
 		
 		def xmlin = new XmlSlurper().parseText(element.toString())
 		
+		log.trace "step1"
+		//def xmlout = new OMElementBuilder("http://example")
 		def xmlout = new OMElementBuilder()
-		
+		log.trace "step2"
 		xmlout.getDateResponse() {
-			currentDate( new Date() )
-			germanLocale( getDateInstance(FULL, GERMAN).format(new Date()))
+			currentDate( new Date().toString() )
+			germanLocale( getDateInstance(FULL, GERMAN).format(new Date()) )
 		}
+		log.trace "step3"
 		
 		xmlout.result
     }
 
     void update(OMElement element) {
 		
+		log.trace "update Method"
+		log.info element.toString()
+
 		def xmlin = new XmlSlurper().parseText(element.toString())
 
     }
