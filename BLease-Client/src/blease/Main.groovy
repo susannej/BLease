b4957159f29af051a16aaa049d6ab397aeeddd98
susannej <susanne_j@gmx.de>
2011-12-22 23:46:28 +0100

    very simple client with a connection to the server

--- BLease-Client/src/blease/Main.groovy
+++ BLease-Client/src/blease/Main.groovy
@@ -5,15 +5,19 @@ import wslite.soap.*
 class Main {
 
 	static void main(args) {
-		def client = new SOAPClient('http://localhost:8080/axis2/services/BLeaseServer')
-		def response = client.send(SOAPAction:'/GetDate') {
+		
+		def client = new SOAPClient('http://localhost:8080/axis2/services/PingService')
+		//def client = new SOAPClient('http://localhost:8080/axis2/services/StockQuoteService')
+		def response = client.send(SOAPAction:'getDate') {
 			body {
-				GetMothersDay('xmlns':'http://www.27seconds.com/Holidays/US/Dates/') {
+				getMothersDay('xmlns':'http://www.27seconds.com/Holidays/US/Dates/') {
 					year(2011)
 				}
 			}
 		}
-		println response
+		
+		println response.getDateResponse.currentDate.text()
+		println response.getDateResponse.germanLocale.text()
 	}
 	
 }
